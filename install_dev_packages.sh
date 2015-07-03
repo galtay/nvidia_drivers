ec0180f91ba941490f88eb89053370fddd388c0a
Gabriel Altay <gabriel.altay@gmail.com>
2015-07-03 12:40:14 -0500

    removed duplicate

--- install_dev_packages.sh
+++ install_dev_packages.sh
@@ -2,5 +2,5 @@ sudo apt-get install -y build-essential
 sudo apt-get install -y emacs24 texlive imagemagick gfortran libsqlite3-dev
 sudo apt-get install -y zlib1g-dev libbz2-dev libssl-dev libreadline-dev
 sudo apt-get install -y libblas-dev liblapack-dev libatlas-dev
-sudo apt-get install -y libpng-dev dvipng libfreetype6-dev qt4-dev-tools
+sudo apt-get install -y libpng-dev dvipng libfreetype6-dev 
 sudo apt-get install -y qt4-dev-tools libqt4-dev
